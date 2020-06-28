b0fd353c78654e22df61d4d84cf2bfc91cff42e1
David Carmona <davidcarmona@gmail.com>
2020-06-27 21:25:32 -0600

    add models/burger.js

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,42 @@
-const connection = require("./connecion.js");
-
+const connection = require("./connection.js");
 
 const orm = {
-    selectAll: (cb) => {
-        connection.query("select * from burgers", function (err,data) {
-            if (err) {
-                console.log(`Select all function not working. Contact programmer.`)
-            }
-            cb(data)
-        })
-    }
-}
+  selectAll: (table, cb) => {
+    connection.query(`select * from ${table};`, function (err, data) {
+      if (err) {
+        console.log(`Select all function not working. Contact programmer.`);
+        res.status(500).end();
+      }
+      cb(data);
+    });
+  },
+
+  insertOne: (table, col, val, cb) => {
+    connection.query(
+      `insert into ${table}(${col.toSting()}) values(${val})`,
+      function (err, data) {
+        if (err) {
+          console.log(`Update function not working. Contact programmer.`);
+          res.status(500).end();
+        }
+        cb(data);
+      }
+    );
+  },
+
+  updateOne: (table, val, condition, cb) => {
+    connection.query(`update ${table} set ${val} where ${condition}`, function (
+      err,
+      data
+    ) {
+      if (err) {
+        console.log(`Update function not working. Contact programmer.`);
+        res.status(500).end();
+      }
+      cb(data);
+    });
+  },
+};
+
+// export orm to use in model
+module.exports = orm;
