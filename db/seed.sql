24123047a178e86d6edf4b2948131992edbbf136
David Carmona <davidcarmona@gmail.com>
2020-07-08 21:32:58 -0600

    add style and color

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
 -- Adding data to bugers table
 
 INSERT INTO burgers ("burger","devoured")
-VALUES("Cheeseburger",0),("Bacon Burger",0),("Bacon CheeseBurger",1),("Jalapeno Buger",0)
+VALUES("Cheeseburger",0),("Bacon Burger",0),("Bacon CheeseBurger",1),("Jalapeno Burger",0)
