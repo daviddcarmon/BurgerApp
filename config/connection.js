a3947a73522253a5644604e62a53de68d189454d
David Carmona <davidcarmona@gmail.com>
2020-07-08 20:37:45 -0600

    move connection code

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,27 @@
 // require in mysql
 const mysql = require("mysql");
 
-let connection;
 // create connection to database using mysql
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "PassWord",
+  database: "burger_db",
+});
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "PassWord",
-    database: "burger_db",
+  // making the connection to database
+  connection.connect((err) => {
+    if (err) {
+      console.log(`Not Connected, check code! ${err.stack}`);
+    }
+    console.log(
+      `Listening on http://localhost:${connection.port} connectionId ${connection.threadId}`
+    );
   });
 }
-
-// making the connection to database
-connection.connect((err) => {
-  if (err) {
-    console.log(`Not Connected, check code! ${err.stack}`);
-  }
-  console.log(
-    `Listening on http://localhost:${connection.port} connectionId ${connection.threadId}`
-  );
-});
-
 // export connection to use in ORM
 module.exports = connection;
