c8bf53364ebad4b707c6106bf5191375113c7876
David Carmona <davidcarmona@gmail.com>
2020-07-08 20:35:18 -0600

    move connection code

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 // require in mysql
 const mysql = require("mysql");
 
+const connection
 // create connection to database using mysql
 
 if (process.env.JAWSDB_URL) {
-  let connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
